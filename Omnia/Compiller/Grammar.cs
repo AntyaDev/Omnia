a22c436391a4c339109f91cd21e0a4df63f2c0ad
Antya Dev <antyadev@gmail.com>
2014-01-05 14:32:43 +0200

    Added support of "open" expression to grammar.

--- Omnia/Compiller/Grammar.cs
+++ Omnia/Compiller/Grammar.cs
@@ -21,20 +21,33 @@ namespace Omnia.Compiller
             var comma = ToTerm(",", "Comma");
             var lPar = ToTerm("(");
             var rPar = ToTerm(")");
-            
+            var open = ToTerm("open", "Open");
+
+            var program = new NonTerminal("Program");
             var expr = new NonTerminal("Expr");
             var term = new NonTerminal("Term");
+
+            var openExpr = new NonTerminal("OpenExpr");
+            var openArg = new NonTerminal("OpenArg");
+            var openList = new NonTerminal("OpenList");
+
             var memberAccess = new NonTerminal("MemberAccess");
             var functionCall = new NonTerminal("FunctionCall");
             var argList = new NonTerminal("ArgList");
 
-            argList.Rule = MakeStarRule(argList, comma, expr);
+            program.Rule = expr | openExpr + expr;
+            expr.Rule = term;
+            term.Rule = number | identifier | @string | @char | memberAccess | functionCall;
+
+            openExpr.Rule = open + PreferShiftHere() + lPar + openList + rPar + NewLine;
+            openArg.Rule = MakeStarRule(openArg, dot, identifier);
+            openList.Rule = MakeStarRule(openList, comma, openArg);
+
             memberAccess.Rule = expr + PreferShiftHere() + dot + identifier;
             functionCall.Rule = expr + PreferShiftHere() + lPar + argList + rPar | expr + PreferShiftHere() + argList;
-            term.Rule = number | identifier | @string | @char | memberAccess | functionCall;
-            expr.Rule = term;
+            argList.Rule = MakeStarRule(argList, comma, expr);
 
-            Root = expr;
+            Root = program;
         }
     }
 }
