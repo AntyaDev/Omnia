46b0ed142bb62d0c424116f2b7c0baa1f7d97e05
Antya Dev <antyadev@gmail.com>
2014-01-10 22:49:29 +0200

    Updated grammar to support indentation

--- Omnia/Compiller/Grammar.cs
+++ Omnia/Compiller/Grammar.cs
@@ -11,43 +11,68 @@ namespace Omnia.Compiller
             var number = TerminalFactory.CreateCSharpNumber("Number");
             var @string = TerminalFactory.CreateCSharpString("String");
             var @char = TerminalFactory.CreateCSharpChar("Char");
+            var @bool = ToTerm("true") | "false";
+            var alphaNumeric = new NonTerminal("AlphaNumeric");
+            var literal = new NonTerminal("Literal");
             var singleLineComment = new CommentTerminal("SingleLineComment", "//", "\r", "\n", "\u2085", "\u2028", "\u2029");
             var delimitedComment = new CommentTerminal("DelimitedComment", "/*", "*/");
             NonGrammarTerminals.Add(singleLineComment);
             NonGrammarTerminals.Add(delimitedComment);
 
-            var colon = ToTerm(":", "Colon");
             var dot = ToTerm(".", "Dot");
             var comma = ToTerm(",", "Comma");
-            var lPar = ToTerm("(");
-            var rPar = ToTerm(")");
             var open = ToTerm("open", "Open");
-
             var program = new NonTerminal("Program");
             var expr = new NonTerminal("Expr");
-            var term = new NonTerminal("Term");
+            var line = new NonTerminal("Line");
+            var body = new NonTerminal("Body");
+            var value = new NonTerminal("Val");
+            var assign = new NonTerminal("Assign");
+            var simpleAssignable = new NonTerminal("SimpleAssignable");
+            var assignable = new NonTerminal("Assignable");
 
             var openExpr = new NonTerminal("OpenExpr");
             var openArg = new NonTerminal("OpenArg");
             var openList = new NonTerminal("OpenList");
+            
+            alphaNumeric.Rule = number | @string | @char;
 
-            var memberAccess = new NonTerminal("MemberAccess");
-            var functionCall = new NonTerminal("FunctionCall");
-            var argList = new NonTerminal("ArgList");
+            literal.Rule = alphaNumeric | @bool;
+            
+            expr.Rule = value | assign;
 
-            program.Rule = expr | openExpr + expr;
-            expr.Rule = term;
-            term.Rule = number | identifier | @string | @char | memberAccess | functionCall;
+            line.Rule = expr + Eos;
 
-            openExpr.Rule = open + PreferShiftHere() + lPar + openList + rPar + NewLine;
-            openArg.Rule = MakeStarRule(openArg, dot, identifier);
+            body.Rule = MakePlusRule(body, line);
+
+            openExpr.Rule = open + PreferShiftHere() + "(" + openList + ")" + Eos;
+            
+            openArg.Rule = MakePlusRule(openArg, dot, identifier);
+
             openList.Rule = MakeStarRule(openList, comma, openArg);
 
-            memberAccess.Rule = expr + PreferShiftHere() + dot + identifier;
-            functionCall.Rule = expr + PreferShiftHere() + lPar + argList + rPar | expr + PreferShiftHere() + argList;
-            argList.Rule = MakeStarRule(argList, comma, expr);
+            program.Rule = openExpr + body | body;
+
+            simpleAssignable.Rule = identifier;
+
+            assignable.Rule = simpleAssignable;
+
+            assign.Rule = (assignable + "=" + expr);
+
+            value.Rule = assignable | literal;
+
+            MarkPunctuation("=", "(", ")", ".", ",");
+            MarkTransient(line, expr, literal, alphaNumeric, value, assignable, simpleAssignable);
 
             Root = program;
+        }
+
+        //Make parser indentation-aware
+        public override void CreateTokenFilters(LanguageData language, TokenFilterList filters)
+        {
+            const OutlineOptions options = OutlineOptions.ProduceIndents | OutlineOptions.CheckBraces | OutlineOptions.CheckOperator;
+            var outlineFilter = new CodeOutlineFilter(language.GrammarData, options, null);
+            filters.Add(outlineFilter);
         }
     }
 }
