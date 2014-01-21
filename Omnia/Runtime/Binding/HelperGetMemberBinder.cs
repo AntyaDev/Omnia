c27683e4de8300bc2db39828a35567686e701cfd
Antya Dev <antyadev@gmail.com>
2014-01-21 15:53:18 +0200

    Added a HelpGetMemberBinder.

---
+++ Omnia/Runtime/Binding/HelperGetMemberBinder.cs
@@ -0,0 +1,21 @@
+﻿using System.Dynamic;
+using System.Linq.Expressions;
+
+namespace Omnia.Runtime.Binding
+{
+    class HelperGetMemberBinder : GetMemberBinder
+    {
+        public HelperGetMemberBinder(string name) : base(name, ignoreCase: false)
+        { }
+
+        public override DynamicMetaObject FallbackGetMember(
+            DynamicMetaObject target, DynamicMetaObject errorSuggestion)
+        {
+            return errorSuggestion ?? 
+                new DynamicMetaObject(
+                    Expression.Constant(RuntimeHelper.Sentinel),
+                    target.Restrictions.Merge(
+                    BindingRestrictions.GetTypeRestriction(target.Expression, target.LimitType)));
+        }
+    }
+}
