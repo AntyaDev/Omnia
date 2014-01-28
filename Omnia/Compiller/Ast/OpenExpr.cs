eaf523382c11664c8b5a78bf60156a28af2522d6
Antya Dev <antyadev@gmail.com>
2014-01-28 11:59:39 +0200

    Added OpenExpr to Ast.

---
+++ Omnia/Compiller/Ast/OpenExpr.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Omnia.Compiller.Ast
+{
+    class OpenExpr
+    {
+        public IEnumerable<string> OpenList { get; private set; }
+
+        public OpenExpr(IEnumerable<string> openList)
+        {
+            OpenList = openList;
+        }
+    }
+}
