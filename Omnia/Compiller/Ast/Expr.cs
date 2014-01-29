8cab8237b59cf66ff7aa549c4de7adfb9f21a0db
Antya Dev <antyadev@gmail.com>
2014-01-29 16:49:35 +0200

    Added a base Expr Ast node.

---
+++ Omnia/Compiller/Ast/Expr.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Omnia.Compiller.Ast
+{
+    abstract class Expr
+    {
+        public abstract ExprType ExprType { get; }
+    }
+
+    enum ExprType
+    {
+        Argument,
+        FunctionalCall,
+        Lambda,
+        Literal,
+        Open,
+        Assign
+    }
+}
