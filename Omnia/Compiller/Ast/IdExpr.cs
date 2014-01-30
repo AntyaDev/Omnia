c0bdd00f900dbb4b0bf4e88b089afc81370bf589
Antya Dev <antyadev@gmail.com>
2014-01-30 11:17:31 +0200

    Added an IdExpr to Ast.

---
+++ Omnia/Compiller/Ast/IdExpr.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Omnia.Compiller.Ast
+{
+    class IdExpr : Expr
+    {
+        public override ExprType ExprType { get { return ExprType.Id; } }
+        public string Name { get; private set; }
+
+        public IdExpr(string name)
+        {
+            Name = name;
+        }
+    }
+}
