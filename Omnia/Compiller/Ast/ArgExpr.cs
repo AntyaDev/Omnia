14199aa86f4737c0156ad87cb3e8b270dd907abf
Antya Dev <antyadev@gmail.com>
2014-02-01 15:06:17 +0200

    Added ArgExpr to Ast.

---
+++ Omnia/Compiller/Ast/ArgExpr.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Omnia.Compiller.Ast
+{
+    class ArgExpr : Expr
+    {
+        public override ExprType ExprType { get { return ExprType.Arg; } }
+        public string Name { get; private set; }
+
+        public ArgExpr(string name)
+        {
+            Name = name;
+        }
+    }
+}
