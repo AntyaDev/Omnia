b95c9027a70c87115f6429280fa2e2740e10c8a1
Antya Dev <antyadev@gmail.com>
2014-02-05 00:10:00 +0200

    Added a lambda expression.

---
+++ Omnia/Compiller/Ast/LambdaExpr.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Omnia.Compiller.Ast
+{
+    class LambdaExpr : Expr
+    {
+        public override ExprType ExprType { get { return ExprType.Lambda; } }
+        public IEnumerable<ArgExpr> Args { get; private set; }
+        public IEnumerable<Expr> Body { get; set; }
+
+        public LambdaExpr(IEnumerable<ArgExpr> args, IEnumerable<Expr> body)
+        {
+            Args = args;
+            Body = body;
+        }
+    }
+}
