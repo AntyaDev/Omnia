e9240ce5f5738bb47b5b9492aa32745e2bbe2b72
Antya Dev <antyadev@gmail.com>
2014-02-05 20:08:29 +0200

    Added a functional call AST.

---
+++ Omnia/Compiller/Ast/FunctionCallExpr.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Omnia.Compiller.Ast
+{
+    class FunctionCallExpr : Expr
+    {
+        public override ExprType ExprType { get { return ExprType.FunctionalCall; } }
+        public IEnumerable<Expr> Params { get; private set; }
+
+        public FunctionCallExpr(IEnumerable<Expr> @params)
+        {
+            Params = @params;
+        }
+    }
+}
