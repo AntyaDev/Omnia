343344e1c645b2978debc53953b7d9fe6bea9473
Antya Dev <antyadev@gmail.com>
2014-02-06 12:41:00 +0200

    Added a LiteralExpr to AST.

---
+++ Omnia/Compiller/Ast/LiteralExpr.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Omnia.Compiller.Ast
+{
+    class LiteralExpr : Expr
+    {
+        public override ExprType ExprType { get { return ExprType.Literal; } }
+        public object Value { get; private set; }
+
+        public LiteralExpr(object value)
+        {
+            Value = value;
+        }
+    }
+}
