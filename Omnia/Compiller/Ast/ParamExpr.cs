e7d728245f5008e4afda102090502eb7dcbcdeeb
Antya Dev <antyadev@gmail.com>
2014-02-07 18:23:11 +0200

    Added a ParamExpr to AST.

---
+++ Omnia/Compiller/Ast/ParamExpr.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Omnia.Compiller.Ast
+{
+    class ParamExpr : Expr
+    {
+        public override ExprType ExprType { get { return ExprType.ParamExpr; } }
+        public Expr Value { get; private set; }
+
+        public ParamExpr(Expr value)
+        {
+            Value = value;
+        }
+    }
+}
