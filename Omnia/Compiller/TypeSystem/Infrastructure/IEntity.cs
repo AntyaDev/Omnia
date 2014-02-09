1f278f93055442628dcc5f597727d0658db4d6c8
Antya Dev <antyadev@gmail.com>
2014-02-09 16:49:05 +0200

    Added an IEntity to TypeSystem.

---
+++ Omnia/Compiller/TypeSystem/Infrastructure/IEntity.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Omnia.Compiller.TypeSystem.Infrastructure
+{
+    interface IEntity
+    {
+        string Name { get; }
+        string FullName { get; }
+        EntityType EntityType { get; }
+    }
+}
