b253f5d09ec0ad05ae7cfa72a781f0879d40d976
Antya Dev <antyadev@gmail.com>
2014-02-11 23:57:31 +0200

    Added IEnumerableExtensions for expressions.

---
+++ Omnia/Infrastructure/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Omnia.Infrastructure
+{
+    static class IEnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (T item in enumeration)
+            {
+                action(item);
+            }
+        }
+    }
+}
