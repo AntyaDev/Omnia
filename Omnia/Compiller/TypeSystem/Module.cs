e1ba38ae8c37929cfe9a46d618b1fc0f37be29d8
Antya Dev <antyadev@gmail.com>
2014-02-12 20:47:48 +0200

    Add module to type system.

---
+++ Omnia/Compiller/TypeSystem/Module.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Omnia.Compiller.TypeSystem.Infrastructure;
+
+namespace Omnia.Compiller.TypeSystem
+{
+    class Module : IModule
+    {
+        public string Name { get; private set; }
+
+        public string FullName { get; private set; }
+
+        public EntityType EntityType { get { return EntityType.Module; } }
+
+        public IModule ParentModule { get; private set; }
+
+        public Module(string fullName)
+        {
+            FullName = fullName;
+        }
+
+        public bool Resolve(IEnumerable<IEntity> resultingSet, string name, EntityType typesToConsider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IEntity> GetMembers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
