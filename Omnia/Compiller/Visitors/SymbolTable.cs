761abf3951b04e92bef3caae6e9915a2fff2a4d4
Antya Dev <antyadev@gmail.com>
2014-02-17 00:38:13 +0200

    Added a SymbolTable to Visitors.

---
+++ Omnia/Compiller/Visitors/SymbolTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Omnia.Compiller.Ast;
+using Omnia.Compiller.Infrastructure;
+using Omnia.Compiller.Scopes;
+using Omnia.Compiller.TypeSystem;
+
+namespace Omnia.Compiller.Visitors
+{
+    class SymbolTable : AstVisitor
+    {
+        readonly Scope _scope;
+
+        public SymbolTable(CompillerContext context, Scope scope) : base(context)
+        {
+            _scope = scope;
+        }
+
+        protected override void VisitIdExpr(IdExpr idExpr)
+        {
+            if (_scope.TryGet(idExpr.Name).IsNone) _scope.Put(new LocalVar(idExpr.Name));
+        }
+        
+        protected override void VisitLambdaExpr(LambdaExpr expr)
+        {
+            _scope.Put(new LocalVar("Compiller_Lambda_" + Guid.NewGuid()));
+            _scope.
+            base.VisitLambdaExpr(expr);
+        }
+    }
+}
