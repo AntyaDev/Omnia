65ef4cdc513cd5748bcfd2fc3d3504a3ba0e5fb5
AntyaDve <antyadev@gmail.com>
2014-04-20 14:32:43 +0300

    refactoring

---
+++ Omnia/Compiller/Ast/FunctionCall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Omnia.Compiller.Ast
+{
+    class FunctionCall : Ast
+    {
+        public override AstType AstType { get { return AstType.FunctionCall; } }
+        public IEnumerable<Ast> Params { get; private set; }
+
+        public FunctionCall(IEnumerable<Ast> @params)
+        {
+            Params = @params;
+        }
+    }
+}
