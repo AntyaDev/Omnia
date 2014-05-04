8d6e13d3667dd08fd38ef95efc58bc8393a02cac
AntyaDve <antyadev@gmail.com>
2014-05-05 00:16:58 +0300

    added functionalDef

---
+++ Omnia/Compiller/Ast/FunctionDef.cs
@@ -0,0 +1,15 @@
+﻿namespace Omnia.Compiller.Ast
+{
+    class FunctionDef : Ast
+    {
+        public override AstType AstType { get { return AstType.FunctionDef; } }
+        public string Name { get; private set; }
+        public LamdaDef Lamda { get; private set; }
+
+        public FunctionDef(IdAst name, LamdaDef lamda)
+        {
+            Name = name.Name;
+            Lamda = lamda;
+        }
+    }
+}
