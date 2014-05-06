09b450b5ef7318b558fc9b142ad3fc2b4ca2ff09
AntyaDve <antyadev@gmail.com>
2014-05-06 13:05:29 +0300

    Added a BinExpr.

---
+++ Omnia/Compiller/Ast/BinExpr.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace Omnia.Compiller.Ast
+{
+    class BinExpr : Ast
+    {
+        public Ast Left { get; private set; }
+        public Ast Right { get; private set; }
+        public ExpressionType Operation { get; private set; }
+        public override AstType AstType { get { return AstType.BinExpr; } }
+
+        public BinExpr(Ast left, Ast right, ExpressionType operation)
+        {
+            Left = left;
+            Right = right;
+            Operation = operation;
+        }
+    }
+}
