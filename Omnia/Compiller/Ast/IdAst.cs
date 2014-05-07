63ed6c01ce10cb469775a5f5e5df60b220badd5a
AntyaDve <antyadev@gmail.com>
2014-05-07 11:23:13 +0300

    Added idAst.

---
+++ Omnia/Compiller/Ast/IdAst.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Omnia.Compiller.Ast
+{
+    class IdAst : Ast
+    {
+        public override AstType AstType { get { return AstType.Id; } }
+        public string Name { get; private set; }
+
+        public IdAst(string name)
+        {
+            Name = name;
+        }
+    }
+}
