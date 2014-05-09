c9a2913c12057c8115043ab9380b69def7049e29
AntyaDve <antyadev@gmail.com>
2014-05-09 14:42:22 +0300

    Added LamdaDef

---
+++ Omnia/Compiller/Ast/LamdaDef.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Omnia.Compiller.Ast
+{
+    class LamdaDef : Ast
+    {
+        public override AstType AstType { get { return AstType.Lambda; } }
+        public Param[] Parameters { get; private set; }
+        public Ast Body { get; set; }
+
+        public LamdaDef(Param[] parameters, Ast body)
+        {
+            Parameters = parameters;
+            Body = body;
+        }
+    }
+}
