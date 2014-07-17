4c6d92a578f8cbeb91fd540d656b762639e46681
AntyaDve <antyadev@gmail.com>
2014-07-18 02:02:22 +0300

    Added OmniaObject

---
+++ Omnia/Compiller/TypeGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Omnia.Compiller
+{
+    static class TypeGenerator
+    {
+        static readonly Lazy<ModuleBuilder> Module = new Lazy<ModuleBuilder>(() =>
+        {
+            var name = new AssemblyName("RuntimeTypes");
+            var assembly = AppDomain.CurrentDomain.DefineDynamicAssembly(name, AssemblyBuilderAccess.Run);
+            return assembly.DefineDynamicModule("RuntimeTypes");
+        });
+
+        public static Type CreateType(string name)
+        {
+            var type = Module.Value.DefineType(name, TypeAttributes.Public, typeof(OmniaObject));
+            return type.CreateType();
+        }
+    }
+}
