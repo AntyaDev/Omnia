4e6f98eebd6e9f9cbb486506e5a6ecb39990f22c
AntyaDve <antyadev@gmail.com>
2014-07-21 11:03:53 +0300

    added pipe call ast.

---
+++ Omnia/Compiller/Ast/PipeCall.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Omnia.Compiller.Ast
+{
+    class PipeCall : Ast
+    {
+        public override AstType AstType { get { return AstType.PipeCall; } }
+        public ObjExpr Function { get; private set; }
+        public Arg Argument { get; private set; }
+
+        public PipeCall(ObjExpr function, Arg arg)
+        {
+            Function = function;
+            Argument = arg;
+        }
+    }
+}
